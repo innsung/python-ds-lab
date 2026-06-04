3935b7fc1f10fa0ac5a6203adc515f7a8890db72
innsung <vvcluo87@gmail.com>
2026-06-04 16:35:10 +0900

    2026-06-04 commit

---
+++ python2/test.py
@@ -0,0 +1,16 @@
+# 변수 a, b에 각각 10, 20을 할당하여 출력
+a = 10
+b = 20
+
+print(a)
+print(b)
+print(a+b)
+
+# 문자열 변수 정의 및 출력
+str1 = "Hello~"
+str2 = "Python"
+
+print(type(str1), type(str2))
+print(str1 + str2)
+print('-' * 50)
+print(str1 * 5)
